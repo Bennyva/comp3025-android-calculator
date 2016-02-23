0635cc5250080b5643c03577f15a78403d3aba7c
Benjamin <benjaminvanarragon@gmail.com>
2016-02-23 18:46:10 -0500

    updated git ignore pls

--- app/src/main/java/comp3025/week1/test/MainActivity.java
+++ app/src/main/java/comp3025/week1/test/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView _welcomeLabel;
     private Button _btnClickMe;
     private EditText _userInputTextBox;
-//overriding the oncreate  
+//overriding the oncreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
